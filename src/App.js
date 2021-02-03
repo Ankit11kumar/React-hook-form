df10522b69aa33410fe4df39bcf61c1ad8caff53
Ankit Kumar <ankit3612kumar@gmail.com>
2021-02-03 07:28:02 +0530

    Demo of react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+
+  // validation function (For custom validation)
+  const validatePassword = (value) => {
+    if (value.length < 6) {
+      return "Password should be at-least 6 characters.";
+    } else if (
+      !/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(?=.*[!@#$*])/.test(value)
+    ) {
+      return "Password should contain at least one uppercase letter, lowercase letter, digit, and special symbol.";
+    }
+    return true;
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control ">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            ref={register({
+              required: "Email is required.",
+              pattern: {
+                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                message: "Email is not valid.",
+              },
+            })}
+          />
+          {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+        </div>
+        <div className="form-control">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            ref={register({
+              required: "Password is required.",
+              // minLength: {
+              //   value: 6,
+              //   message: "Password should be at-least 6 characters.",
+              // },
+              validate: validatePassword,  // For custom validation, we can create our own validation function
+            })}
+          />
+          {errors.password && (
+            <p className="errorMsg">{errors.password.message}</p>
+          )}
+        </div>
+        <div className="form-control">
+          <label></label>
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
   );
 }
-
-export default App;
